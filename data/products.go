4e155a4cf238673e0db4d95a360c8b58de467ab1
Shane Oh <ohshane71@gmail.com>
2020-12-28 18:37:42 +0900

    new file:   data/products.go

---
+++ data/products.go
@@ -0,0 +1,35 @@
+package data
+
+import "time"
+
+type Product struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float32
+	SKU         string
+	CreatedOn   string
+	UpdatedOn   string
+	DeletedOn   string
+}
+
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Forthy milky coffee",
+		Price:       2.45,
+		SKU:         "cl001",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and string coffee without milk",
+		Price:       1.99,
+		SKU:         "ce001",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
