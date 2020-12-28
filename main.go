c8ff6edf0783378c63a34f7cb2cba31571b47ab5
Shane Oh <ohshane71@gmail.com>
2020-12-28 17:37:22 +0900

    added goodbye handler and graceful shutdown

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/ohshane71/go-http-tut/handlers"
 )
@@ -11,9 +14,34 @@ import (
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	helloHandler := handlers.NewHello(l)
+	goodbyeHandler := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", helloHandler)
+	sm.Handle("/goodbye", goodbyeHandler)
 
-	http.ListenAndServe(":9090", sm)
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
